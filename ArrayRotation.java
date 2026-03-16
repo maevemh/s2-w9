b709d0cf95ad93ed945e2c4b929d04db117ccc8c
AHS CS Student <ahs-cs@ausd>
2026-03-16 15:02:00 -0700

    df

--- ArrayRotation.java
+++ ArrayRotation.java
@@ -2,13 +2,44 @@
 //See Instrucitons:
 // http://apcsa-book.ausdk12.org/apcsa/r/cur/c4/L23_2D_arrays/exercises3.html?topic=c4%2FL23_2D_arrays.topic
 public class ArrayRotation{
-    private int[] nums;
+    private int[][] nums;
     
     public ArrayRotation(int n){
+        nums = new int[n][n];
+        int y=0;
+        for(int r=0; r<nums.length; r++){
+            for(int c=0; c<nums[0].length; c++){
+                nums[r][c]= ++y;
+            }
+        }
 
     }
 
-    public void rotate(){
 
+    public void rotate(){
+        int[][] temp = new int[nums.length][nums[0].length];
+        int y=0;
+        for(int c=(nums[0].length-1); c>=0; c--){
+            for(int r=0; r<nums.length; r++){
+                temp[r][c]= ++y;
+
+            }
+        }
+        nums=temp;
+
+    }
+    public void print(){
+         for(int r=0; r<nums.length; r++){
+            for(int c=0; c<nums[0].length; c++){
+               System.out.print(nums[r][c] + " ");
+            }
+            System.out.println(" ");
+        }
+
+    }
+    public static void main(String[] args) {
+        ArrayRotation a = new ArrayRotation(3);
+        a.rotate();
+        a.print();
     }
 }
--- GameBoard.java
+++ GameBoard.java
@@ -1,15 +1,39 @@
 // See Instructions:
 // http://apcsa-book.ausdk12.org/apcsa/r/cur/c4/L23_2D_arrays/exercises0.html?topic=c4%2FL23_2D_arrays.topic
 public class GameBoard {
+    private String[][] board;
     public GameBoard(int rows, int cols){
+        board = new String[rows][cols];
+        for(int r=0; r<board.length; r++){
+            for(int c=0; c<board[0].length; c++){
+                board[r][c] = "e";
+            }
+        }
 
     }
 
     public void setCell(int player, int row, int col, String c){
+        if(player==0){
+            board[row][col]=c;
+
+        }else{
+            board[board.length-1-row][board[0].length-1-col]=c;
+        }
 
     }
 
     public void print(int player){
+         for(int r=0; r<board.length; r++){
+            for(int c=0; c<board[0].length; c++){
+               System.out.print(board[r][c] + " ");
+            }
+            System.out.println(" ");
+        }
 
+    }
+    public static void main(String[] args) {
+        GameBoard g = new GameBoard(3, 5);
+        g.setCell(1, 2, 1, "w");
+        g.print(0);
     }
 }
